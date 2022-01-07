55c9a4ea20ee1b6fc894ea7582eacc1055f58dec
Abidin <userabidin97gmail.com>
2022-01-07 09:21:48 +0700

    Perubahan index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const flash = require("express-flash");
 
 const app = express();
-const PORT = process.env.PORT || 5007;
+const PORT = process.env.PORT || 3000;
 let isLogin = true;
 let blogs = [
   {
